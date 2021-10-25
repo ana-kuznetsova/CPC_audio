15be93321d07ef66d9ff549317acc5bbf0df75a5
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-10-25 14:59:34 -0400

    debug

--- cpc/train.py
+++ cpc/train.py
@@ -109,7 +109,6 @@ def trainStep(dataLoader,
         n_examples += batchData.size(0)
         batchData = batchData.cuda(non_blocking=True)
         label = label.cuda(non_blocking=True)
-        print(f"DEBUG: batchData {batchData}, label {label}")
         c_feature, encoded_data, label = cpcModel(batchData, label)
         allLosses, allAcc, _ = cpcCriterion(c_feature, encoded_data, label, None)
         totLoss = allLosses.sum()
