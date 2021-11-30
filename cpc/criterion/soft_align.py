038e87f24ed1ae7db617ef8d5a7e0d809c75f7d2
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-11-30 16:04:22 -0500

    dev

--- cpc/criterion/soft_align.py
+++ cpc/criterion/soft_align.py
@@ -355,6 +355,8 @@ class CPCUnsupersivedCriterion(BaseCriterion):
         pos_log_scores = positives @ predictions / sampledNegs.size(-1)
         avg_pos_log_scores = torch.mean(pos_log_scores, -1, keepdim=True) #Average across K dim
         s_target = torch.mean(torch.mul(avg_pos_log_scores, positives), 2)
+        snorm = torch.linalg.norm(s_target, -1)
+        s_target/=snorm
         e_noise  = torch.mean(predictions, -1) - s_target
         print(f"DEV: enoise:{e_noise.shape}")
 
