5de89dfa17100e1906be802042eb09ef0c32d7ae
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-12-06 17:02:49 -0500

    dev

--- cpc/criterion/soft_align.py
+++ cpc/criterion/soft_align.py
@@ -391,7 +391,7 @@ class CPCUnsupersivedCriterion(BaseCriterion):
         #print(f"DEV: log_scores: {log_scores.shape}")
         
         log_scores = log_scores.view(batchSize*windowSize, self.nMatched, nPredicts)
-        print(f"DEV: log_scores: {log_scores}")
+        #print(f"DEV: log_scores: {log_scores}")
         # print('ls-stats', log_scores.mean().item(), log_scores.std().item())
         if self.masq_buffer is not None:
             masq_buffer = self.masq_buffer
